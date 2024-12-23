c3a9de3bbcb2eb9cc790555fae10748dcefa1fda
Gözde Hızır <goz.akkaya@gmail.com>
2024-12-23 18:01:14 +0300

    Resolved merge conflicts in recipeService.js

--- src/modules/recipes/recipeService.js
+++ src/modules/recipes/recipeService.js
@@ -2,8 +2,14 @@ import { fetchData } from "../../utils";
 
 export const BD_BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 
-export async function fetchRecipesByIngredient(idMeal) {
-  const data = await fetchData(`${BD_BASE_URL}/filter.php?i=`, idMeal);
+export async function fetchRecipesByIngredient(query) {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${query}`
+  );
+  if (!response.ok) {
+    throw Error("An error occured when fetching data.");
+  }
+  const data = await response.json();
   return data.meals;
 }
 
