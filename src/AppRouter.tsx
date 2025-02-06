8942d0cfad6b60114fd521227446888e1c7f6780
Gözde Hızır <goz.akkaya@gmail.com>
2025-02-06 16:47:47 +0300

    Fixes have been implemented.

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,7 +4,7 @@ import { RecipeDetails } from "./modules/recipes/RecipeDetails";
 import { ProtectedRoute } from "./ProtectedRoute";
 import { NewRecipeForm } from "./modules/recipes/NewRecipeForm";
 import { Login } from "./modules/user/Login";
-import { CategoryDetailsComponent } from "./modules/recipes/Categories";
+import { RecipeList } from "./modules/recipes/RecipeList";
 import { CategoryDetails } from "./modules/recipes/CategoryDetails";
 import { Register } from "./modules/user/Register";
 import { FeaturedRecipes } from "./modules/recipes/FeaturedRecipes";
@@ -45,7 +45,7 @@ export const AppRouter = () => {
           element={
             <>
               <SearchBar />
-              <CategoryDetailsComponent />
+              <RecipeList />
             </>
           }
         />
